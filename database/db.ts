a8614fda0fe410429e575e54e7ae8d3dc9ff43fa
Vinit-S-Patil <patil.shubham1212@gmail.com>
2025-01-21 11:24:11 +0530

    commit

--- database/db.ts
+++ database/db.ts
@@ -3,22 +3,13 @@ let MDB: Connection | null = null;
 
 export const connectDB = async (): Promise<Connection> => {
 
-  // const userName = process.env.MONGO_USERNAME
-  // const password = process.env.MONGO_PASSWORD
-  // const dbName = process.env.MONGO_DB_NAME
-
-  const userName = "ShubhamSP"
-  const password = "VsXLbJWnoxyWA5pa"
   const dbName = "SocialMediaDB"
-
 
 
   if (MDB) {
     return MDB;
   }
 
-
-  // let connectionUrl = `mongodb+srv://${userName}:${password}@cluster0.teccyhd.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
   let connectionUrl = `mongodb://localhost:27017/${dbName}`
   try {
 
