e8316424b3f59c0008fa1487ef91ae30cd1b5089
Vinit-S-Patil <patil.shubham1212@gmail.com>
2025-01-21 11:34:47 +0530

    commit

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -10,7 +10,7 @@ import { PassportModule } from '@nestjs/passport';
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: "f5ac5fbe23b938af4e83d5c89fa3e71ab8d38fef620e72d4c0b2b98a8dd8e473a2b75a7c2e9b4eec1d938b2e6fdbdb64e4d0d9e65f607bfe8bb7c980a6b53c67",
-      signOptions: { expiresIn: "1h" },
+      signOptions: { expiresIn: "8h" },
     }),
   ],
   controllers: [PostController],
